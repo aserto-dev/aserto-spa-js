8d1366d3ab30472c75eebbcaca0f0e36d49e9501
Omri Gazitt <ogazitt@gmail.com>
2020-12-20 22:06:56 -0800

    removed react hooks

--- src/api.js
+++ src/api.js
@@ -2,10 +2,6 @@
 // exports:
 //   get - make a get call
 //   post - make a post call
-//   useApi - a React effect that returns { get, post }
-//   [note: using the useApi effect is the proper way to structure API calls going forward]
-
-import { useCallback } from 'react'
 
 export async function get(token, path, headers = {}) { 
   // construct API service URL
@@ -58,31 +54,5 @@ export async function post(token, path, data, headers = {}) {
   } catch (error) {
     console.error(error);
     return [null, error];
-  }
-}
-
-// define a custom effect that wraps the auth0 effect, gets a token, and calls the API method
-export function useApi() {
-  // create a callback around the GET call
-  const callGet = useCallback((...p) => {
-    async function callGet(token, path, headers = {}) {
-      const [response, error] = await get(token, path, headers);
-      return [response, error];
-    }
-    return callGet(...p);
-  }, []);
-
-  // create a callback around the POST call
-  const callPost = useCallback((...p) => {
-    async function callPost(token, ...p) {
-      const [response, error] = await post(token, ...p);
-      return [response, error];
-    }
-    return callPost(...p);
-  }, []);
-
-  return {
-    get: callGet,
-    post: callPost
   }
 }
