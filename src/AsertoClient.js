744a558d0ab1af8313996fcb5b8b02f56035fee6
Omri Gazitt <ogazitt@gmail.com>
2020-12-24 13:49:05 -0800

    error checking for options struct, changed default access map endpoint to __accessmap

--- src/AsertoClient.js
+++ src/AsertoClient.js
@@ -2,11 +2,14 @@ import { get } from './api'
 
 export default class AsertoClient {
   constructor(options) {
+    if (!options || !options.token) {
+      throw new Error('AsertoClient: must provide access token');
+    }
     this.token = options.token;
-    this.endpoint = options.endpoint;
+    this.endpoint = options.endpoint || '__accessmap';
   }
 
-  async getAuthorizationMap() {
-    return await get(this.token, 'authzmap');
+  async getAccessMap() {
+    return await get(this.token, this.endpoint);
   }
 }
